c7cb5a56c45022e9fa5ddf5dad1d16a41d644b7e
Yui-Iida <92433326+Yui-Iida@users.noreply.github.com>
2022-05-06 18:09:03 +0200

    final commit

--- README.md
+++ README.md
@@ -4,19 +4,30 @@
 
 ## Project Description
 
-With starter code, I have built a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+[Project Summary on udacity]
+
+This project aims to give you real-world scenarios of manipulating the DOM. The functionality you will be using serves two purposes: to prepare you for appending dynamically added data to the DOM, and to show you how javascript can improve the usability of an otherwise static site. This project barely touches the surface of what is possible, but it does use some incredibly common events, methods, and logic.
+
+For this project, refactor and test as much as possible while you are building. You should figure for every piece of functionality you add, you will likely spend just as much time testing and refactoring your code. If it takes you 3 hours to figure out the logic, it should likely take you another 3 hours determining that you wrote the best code possible. As your skills improve, this process will feel more natural. Make sure to remove any debugging code from your final submission.
+
+... With starter code, I have built a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
 
 What I add functionally on JavaScript are:
 
 - navigation
-- scroll to section
+- smoothly scroll to section
 - highlights section in viewport upon scrolling
 - hide/show navigation while scrolling
 - implement styles
 
-note: 'hightlights section in viewport upon scrolling' is helped by a mentor on Knowledge 
-https://knowledge.udacity.com/questions/834057?utm_campaign=ret_600_auto_ndxxx_knowledge-answer-created_na&utm_source=blueshift&utm_medium=email&utm_content=ret_600_auto_ndxxx_knowledge-answer-created_na&bsft_clkid=ce382959-cf7a-4159-acf6-711497970375&bsft_uid=26c71313-b912-4455-9310-df30fad544ab&bsft_mid=c4bc11fa-1820-46b0-88ec-bf14b2a54c2d&bsft_eid=22b8f7b6-5eac-66ee-cf9f-0d5b86b9fddc&bsft_txnid=0fbf8ef1-53e1-4f66-9ee9-c8ba7f7b1f5f&bsft_mime_type=html&bsft_ek=2022-04-28T18%3A52%3A31Z&bsft_aaid=8d7e276e-4a10-41b2-8868-423fe96dd6b2&bsft_lx=1&bsft_tv=1#838647
+### Copyright
 
+All rights are reserved by Yui Iida.
 
+note: 'hightlights section in viewport upon scrolling' is helped by a mentor on Knowledge
+https://knowledge.udacity.com/questions/834057
+
+note: 'nav link scrolling in JS' is helped by a mentor on Knowledge
+https://knowledge.udacity.com/questions/840293
 
 Some styles and functions will be added in the future.
--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@
 // //  *
 //  */
 
-const sections = Array.from(document.querySelectorAll('section'));
+const sections = document.querySelectorAll('section');
 const navBar = document.querySelector('#navbar__list');
 
 // /**
@@ -32,6 +32,21 @@ const createNavItem = () => {
 
     const setNavBar = () => {
       navItem.innerHTML = `<a href="#${href}" class="nav_item" data-nav="${href}">${navName}</a>`;
+
+      navItem.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        // let toSection = section.getAttribute('id'); => fixed to the next line
+        let toSection = e.target.getAttribute('data-nav');
+        // console.log(toSection);
+
+        document.getElementById(toSection).scrollIntoView({
+          behavior: 'smooth',
+          block: 'end',
+          inline: 'nearest',
+        });
+      });
+
       navBar.appendChild(navItem);
     };
     setNavBar();
@@ -42,26 +57,33 @@ createNavItem();
 
 // // does not show nav bar when it is scrolling
 let timeOut;
+const header = document.querySelector('.page__header');
 
 window.addEventListener('scroll', function () {
-  navBar.classList.add('navbar__hide');
+  header.classList.add('navbar__hide');
 
   timeOut = this.setTimeout(function () {
-    navBar.classList.remove('navbar__hide');
+    header.classList.remove('navbar__hide');
   }, 1000);
 });
 
-// highlighting navigation item when it is on viewport
+//  highlighting navigation item when it is on viewport
 
-const activeSection = (elem) => {
-  if (document.querySelector('.menu__link__active') !== null) {
-    navBar.classList.remove('menu__link__active');
-  }
-  const currentSection = document.querySelector(
-    `a[href='#${section.getAttribute('id')}']`
-  );
-  currentSection.classList.add('menu__link__active');
-};
+// const activeSection = (elem) => {
+//   if (document.querySelector('.menu__link__active') !== null) {
+//     navBar.classList.remove('menu__link__active');
+//   }
+//   const currentSection = document.querySelector(
+//     `a[href='#${section.getAttribute('id')}']`
+//   );
+
+//   // console.log(currentSection);
+//   currentSection.classList.add('menu__link__active');
+// };
+
+//  activeSection();
+
+// // add styles to navigation item when it is on viewport
 
 const intersect = (entries) => {
   for (entry of entries) {
@@ -71,7 +93,6 @@ const intersect = (entries) => {
     if (entry.isIntersecting) {
       navListElement.classList.add('menu__link__active');
       entry.target.classList.add('your-active-class');
-      // activeSection(entry.target);
     } else {
       navListElement.classList.remove('menu__link__active');
       entry.target.classList.remove('your-active-class');
